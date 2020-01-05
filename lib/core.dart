ddb84b9e006d62c2ab85e4e62c3bf619d16b6fb4
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2020-01-05 15:12:20 -0500

    Core library

---
+++ lib/core.dart
@@ -0,0 +1,12 @@
+library quantum_core;
+
+export 'core/data.dart';
+export 'core/break.dart';
+export 'core/engine.dart';
+export 'core/output.dart';
+export 'core/process.dart';
+export 'core/processor.dart';
+export 'core/quantum.dart';
+export 'core/rules.dart';
+export 'core/structure.dart';
+export 'core/trace.dart';
