8ca997f0d40f816c5fd3445cfe74ae497d2effd8
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2020-01-06 01:31:34 -0500

    Section States ready

--- lib/src/pages/section_states.dart
+++ lib/src/pages/section_states.dart
@@ -1,8 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quantum/core.dart';
+import 'package:quantum/src/models/output_model.dart';
+
+double tileSize = 40;
 
 class SectionStates extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var quantums = Provider.of<OutputModel>(context).quantums;
+    var style = TextStyle(
+      color: Theme.of(context).primaryColor,
+      fontWeight: FontWeight.bold,
+      fontSize: Theme.of(context).textTheme.subhead.fontSize,
+    );
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: tileSize * 1.5),
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return Divider();
+            },
+            itemCount: quantums.length,
+            itemBuilder: (context, index) {
+              return QuantumItem(
+                traces: quantums[index].traces,
+              );
+            },
+          ),
+        ),
+        Container(
+          height: tileSize * 1.5,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(child: Center(child: Text('Process', style: style))),
+              Expanded(child: Center(child: Text('Advance', style: style))),
+              Expanded(child: Center(child: Text('State', style: style))),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class QuantumItem extends StatelessWidget {
+  const QuantumItem({Key key, @required this.traces}) : super(key: key);
+
+  final List<Trace> traces;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: tileSize * traces.length.toDouble(),
+      child: Column(
+        children: traces
+            .map(
+              (Trace trace) => Flexible(
+                child: TraceItem(trace: trace),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class TraceItem extends StatelessWidget {
+  const TraceItem({Key key, this.trace}) : super(key: key);
+
+  final Trace trace;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget traceWidget;
+
+    if (trace.isEmpty) {
+      traceWidget = Center(
+        child: Text('Tiempo muerto'),
+      );
+    } else {
+      traceWidget = Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('P'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(trace.process),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(child: Text(trace.advance)),
+          ),
+          Expanded(
+            child: Center(child: Text(trace.status + trace.idBreak)),
+          ),
+        ],
+      );
+    }
+
+    return Container(
+      height: tileSize,
+      child: traceWidget,
+    );
+    ;
   }
 }
