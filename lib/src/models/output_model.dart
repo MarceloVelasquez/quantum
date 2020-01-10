c89e5b0386aff6c53c3c7d35e2aa49511c40cdfe
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2020-01-10 12:42:48 -0500

    inAction data ready for chart

--- lib/src/models/output_model.dart
+++ lib/src/models/output_model.dart
@@ -5,6 +5,7 @@ import 'package:quantum/core.dart';
 class OutputModel with ChangeNotifier {
   Output _output;
   Engine _engine = Engine();
+  List<DataChart> dataChart = [];
 
   Output get output => _output;
   List<Quantum> get quantums => _output.quantums;
@@ -12,5 +13,19 @@ class OutputModel with ChangeNotifier {
 
   void executeData(Data data) {
     _output = _engine.generate(data);
+    var values = _output.traces
+        .where((trace) => trace.status == Status.inAction)
+        .map((f) => f.id)
+        .toList();
+    for (var i = 1; i <= values.length; i++) {
+      dataChart.add(DataChart(i.toString(), values[i - 1]));
+    }
   }
+}
+
+class DataChart {
+  String string;
+  int value;
+
+  DataChart(this.string, this.value);
 }
--- lib/src/models/structure_model.dart
+++ lib/src/models/structure_model.dart
@@ -3,36 +3,28 @@ import 'package:quantum/core.dart';
 
 class StructureModel with ChangeNotifier {
   List<Trace> _traces;
-
-  List<int> _newed = [];
-  List<int> _ready = [];
-  List<int> _inAction = [];
-  List<int> _locked = [];
-  List<int> _suspended = [];
-  List<int> _finished = [];
-  List<int> _lost = [];
-
+  Trace _trace;
   int _counter;
 
-  Trace _trace;
   Trace get actualTrace => _trace;
   bool get isDone => _traces.length == _counter;
 
   Map<Status, List<int>> structures;
 
   StructureModel(this._traces) {
-    var newed = _traces.where((trace) => trace.status == Status.newed);
-    _newed = newed.map((trace) => trace.id).toList();
-    _counter = newed.length;
     structures = {
-      Status.newed: _newed,
-      Status.ready: _ready,
-      Status.inAction: _inAction,
-      Status.locked: _locked,
-      Status.suspended: _suspended,
-      Status.finished: _finished,
-      Status.lost: _lost,
+      Status.newed: [],
+      Status.ready: [],
+      Status.inAction: [],
+      Status.locked: [],
+      Status.suspended: [],
+      Status.finished: [],
+      Status.lost: [],
     };
+
+    var newed = _traces.where((trace) => trace.status == Status.newed);
+    structures[Status.newed] = newed.map((trace) => trace.id).toList();
+    _counter = newed.length;
   }
 
   void removeProcess(int id) {
