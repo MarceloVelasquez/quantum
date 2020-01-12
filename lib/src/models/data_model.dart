8d4c0a025cb5c35c29de6bd5733f35eef70834b4
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2020-01-11 22:56:23 -0500

    Refactor buttons

--- lib/src/models/data_model.dart
+++ lib/src/models/data_model.dart
@@ -58,22 +58,3 @@ class DataModel with ChangeNotifier {
 
   Data data() => validate() ? _builder.build() : null;
 }
-
-// addProcess(Process(1, 4, 4, Breaks([Break(1, 1), Break(2, 2)]), 1));
-// [
-//   Process(1, 4, 4, Breaks([Break(1, 1), Break(2, 2)]), 1),
-//   Process(2, 6, 3, Breaks([Break(1, 3), Break(2, 1)]), 3),
-//   Process(3, 3, 4, Breaks([Break(1, 1), Break(2, 3)]), 1),
-//   Process(4, 2, 3, Breaks([Break(1, 2), Break(2, 3)]), 2),
-//   Process(5, 1, 5, Breaks([Break(1, 1), Break(2, 4)]), 3),
-// ];
-// inputRules = InputRules([
-//   StructureHeap(Status.newed),
-//   StructurePriority(Status.ready, [2, 1, 3]),
-//   StructurePriority(Status.locked, [2, 3, 1]),
-//   StructurePriority(Status.suspended, [3, 1, 2]),
-// ]);
-// breakRules = BreakRules([
-//   BreakRule.locked(1, 1),
-//   BreakRule.discontinued(2, 1, 2),
-// ]);
--- lib/src/widgets/buttons.dart
+++ lib/src/widgets/buttons.dart
@@ -7,13 +7,24 @@ import 'package:quantum/src/models/output_model.dart';
 class SimulateButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        Data data = Provider.of<DataModel>(context, listen: false).data();
-        Provider.of<OutputModel>(context, listen: false).executeData(data);
-        Navigator.pushNamed(context, '/simulator');
-      },
-      child: Text('EJECUTAR'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(4),
+          child: RaisedButton(
+            onPressed: () {
+              Data data = Provider.of<DataModel>(context, listen: false).data();
+              Provider.of<OutputModel>(context, listen: false)
+                  .executeData(data);
+              Navigator.pushNamed(context, '/simulator');
+            },
+            color: Theme.of(context).primaryColor,
+            textColor: Colors.white,
+            child: Text('EJECUTAR'),
+          ),
+        ),
+      ],
     );
   }
 }
