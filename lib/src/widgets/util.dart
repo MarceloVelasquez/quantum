3d54859a8b522c4b8b6f83f27bc2a784a569db8c
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2020-01-11 22:58:07 -0500

    Util file to get core data from string

---
+++ lib/src/widgets/util.dart
@@ -0,0 +1,65 @@
+import 'package:quantum/core.dart';
+
+class Util {
+  static List<int> getNumbersFromField(String text) {
+    bool clean = true;
+    var ints = text.trim().split(' ').map((number) {
+      if (int.tryParse(number.trim()) == null) clean = false;
+      return int.tryParse(number.trim());
+    }).toList();
+    return clean ? ints : null;
+  }
+
+  static BreakRules getBreakRules(String text) {
+    List<BreakRule> list = [];
+
+    var aux = text.trim().split(',');
+
+    if (aux == null || aux.length == 0) return null;
+
+    for (var i = 0; i < aux.length; i++) {
+      var rules = Util.getNumbersFromField(aux[i].trim());
+      if (rules == null) return null;
+      BreakRule breakRule = BreakRule.empty();
+      if (rules.length == 2) {
+        breakRule = BreakRule.locked(rules[0], rules[1]);
+      } else if (rules.length == 3) {
+        breakRule = BreakRule.discontinued(rules[0], rules[1], rules[2]);
+      } else
+        return null;
+      list.add(breakRule);
+    }
+
+    return BreakRules(list);
+  }
+
+  static Structure getInputStructure(Status status, String text) {
+    Structure structure;
+    String cleanText = text.trim().toLowerCase();
+
+    switch (cleanText) {
+      case 'cola':
+        structure = StructureQueue(status);
+        break;
+      case 'pila':
+        structure = StructureHeap(status);
+        break;
+      default:
+        var ints = getNumbersFromField(cleanText);
+        structure = ints == null ? null : StructurePriority(status, ints);
+        break;
+    }
+
+    return structure;
+  }
+
+  static Iterable<E> indexed<E, T>(
+      Iterable<T> items, E Function(int index, T item) f) sync* {
+    var index = 0;
+
+    for (final item in items) {
+      yield f(index, item);
+      index = index + 1;
+    }
+  }
+}
